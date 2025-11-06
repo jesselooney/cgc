bc485e482a5ff75754e5057fe82e9c1b7f70396b
Jesse Looney <jesselooney+dev@pm.me>
2025-11-05 21:52:26 -0500

    add some debugging to alloc

--- include/alloc.h
+++ include/alloc.h
@@ -1,10 +1,13 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include <errno.h>
 #include <sys/mman.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+
+#include "debug.h"
 
 //===============================================
 // Members
@@ -68,11 +71,20 @@ static void *ALLOC_FREE_LISTS[ALLOC_MAX_BLOCK_SIZE_EXP] = { NULL };
 //      pool aligned address and bump the top up from there
 void alloc_init()
 {
-    intptr_t ptr =
-        (intptr_t) mmap(NULL, ALLOC_HEAP_SIZE, PROT_READ | PROT_WRITE,
-                        MAP_ANONYMOUS, -1, 0);
-    ALLOC_HEAP_START = (void *) (ptr + (ptr % ALLOC_POOL_SIZE));
+    log_info("alloc_init()");
+    void *ptr = mmap(NULL, ALLOC_HEAP_SIZE, PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (ptr == MAP_FAILED) {
+        log_error("call to mmap failed with errno == %d", errno);
+        exit(errno);
+    }
+
+    log_debug("ptr == %p", (void *) ptr);
+    ALLOC_HEAP_START = (void *) (ptr + ((intptr_t) ptr % ALLOC_POOL_SIZE));
     ALLOC_HEAP_TOP = ALLOC_HEAP_START;
+    log_debug("ALLOC_HEAP_START == ALLOC_HEAP_TOP == %p", ALLOC_HEAP_TOP);
+    log_info("alloc_init() == void");
 }
 
 //  alloc_new(size_t)
@@ -80,16 +92,21 @@ void alloc_init()
 //      rounded up to occupy a block of the next power of 2 size
 void *alloc_new(size_t size)
 {
+    log_info("alloc_new(%ld)", size);
     _log2_ceil_return_t log2_ceil = _alloc_log2_ceil(size);
     int index = log2_ceil.exp;
     int block_size = log2_ceil.pow;
+    log_debug("index == %d", index);
+    log_debug("block_size == %d", block_size);
 
     // if smaller than smallest block, over-allocate in the smallest block size
-    if (index < ALLOC_MIN_BLOCK_SIZE_EXP)
+    if (index < ALLOC_MIN_BLOCK_SIZE_EXP) {
         index = ALLOC_MIN_BLOCK_SIZE_EXP;
-
+        log_info("block size too small; clamping index to %d", index);
+    }
     // Fail if requested size is larger than the largest size class.
     if (index > ALLOC_MAX_BLOCK_SIZE_EXP) {
+        log_info("block size too large; returning NULL");
         // TODO: Add large object handling. 
         return NULL;
     }
@@ -98,13 +115,20 @@ void *alloc_new(size_t size)
 
     // If there is no next block, create one by allocating a pool.
     if (block == NULL) {
+        log_info("no free block in size class; allocating a new pool");
         void *head_block = _alloc_new_pool(block_size);
-        if (head_block == NULL)
+        if (head_block == NULL) {
+            log_info("failed to allocate new pool");
+            log_info("alloc_new(...) == NULL");
             return NULL;        // Failed to allocate the pool.
+        }
         block = head_block;
     }
+    log_debug("block == %p", block);
+
     // Pull `block` off the free list.
     void *next_block = *((void **) block);
+    log_debug("next_block == %p", next_block);
     ALLOC_FREE_LISTS[index] = next_block;
 
     // Null out `block`'s pointer into the free list so we don't give the user
@@ -114,6 +138,7 @@ void *alloc_new(size_t size)
     // Mark `block` as occupied before returning it to the user.
     _alloc_clear_free_bit(block);
 
+    log_info("alloc_new(...) == %p", block);
     return block;
 }
 
@@ -152,31 +177,52 @@ void alloc_set_mark_bit(void *block)
 //      return a pointer to the block at the head of the linked list of blocks on success
 static void *_alloc_new_pool(size_t block_size)
 {
+    log_info("_alloc_new_pool(%ld)", block_size);
+
+    log_debug("ALLOC_HEAP_TOP == %p", ALLOC_HEAP_TOP);
+    log_debug("ALLOC_POOL_SIZE == %d", ALLOC_POOL_SIZE);
+    log_debug("ALLOC_HEAP_START == %p", ALLOC_HEAP_START);
+    log_debug("ALLOC_HEAP_SIZE == %d", ALLOC_HEAP_SIZE);
     if (ALLOC_HEAP_TOP + ALLOC_POOL_SIZE >
-        ALLOC_HEAP_START + ALLOC_HEAP_SIZE)
+        ALLOC_HEAP_START + ALLOC_HEAP_SIZE) {
+        log_info("cannot allocate new pool");
+        log_info("_alloc_new_pool(...) == NULL");
         return NULL;
+    }
 
     pool_t *pool = (pool_t *) ALLOC_HEAP_TOP;
     ALLOC_HEAP_TOP += ALLOC_POOL_SIZE;
+    log_debug("ALLOC_HEAP_TOP == %p", ALLOC_HEAP_TOP);
 
     pool->block_size = block_size;
+    log_debug("pool == %p", pool);
+    log_debug("pool->block_size == %ld", pool->block_size);
+
     // Set all the free bits. This includes the bits corresponding to blocks
     // this header occupies. Since we never add those to the free list, this
     // ensures they will never be touched in any way hereafter.
+    log_debug("BITVEC_SIZE(block_size) == %ld", BITVEC_SIZE(block_size));
     memset(pool->data, ~0, BITVEC_SIZE(block_size));
 
     void *pool_start = pool;
+    void *pool_end = pool_start + ALLOC_POOL_SIZE;
+    void *pool_header_end = pool_start + HEADER_SIZE(block_size);
     void *prev = NULL;
     void *block = pool_start + block_size;
-    for (; block < pool_start + ALLOC_POOL_SIZE; block += block_size) {
-        if (block < pool_start + HEADER_SIZE(block_size))
+    log_info("dividing pool into blocks");
+    log_debug("pool_end == %p", pool_end);
+    log_debug("pool_header_end == %p", pool_header_end);
+    for (; block < pool_end; block += block_size) {
+        if (block < pool_header_end) {
             continue;
+        }
 
         *((void **) block) = prev;
         prev = block;
     }
 
-    return block;
+    log_info("_alloc_new_pool(...) == %p", prev);
+    return prev;
 }
 
 static void _alloc_set_free_bit_by_id(pool_t *pool, size_t block_id)
@@ -187,10 +233,17 @@ static void _alloc_set_free_bit_by_id(pool_t *pool, size_t block_id)
 
 static void _alloc_clear_free_bit(void *block)
 {
+    log_info("_alloc_clear_free_bit(%p)", block);
+
     pool_t *pool = (pool_t *) GET_POOL((intptr_t) block);
+    log_debug("pool == %p", pool);
+    log_debug("pool->block_size = %ld", pool->block_size);
+
     size_t block_id = BLOCK_ID((intptr_t) pool, (intptr_t) block);
     uint8_t *resident_byte = &pool->data[block_id / 8];
     CLEAR_BIT(resident_byte, block_id % 8);
+
+    log_info("_alloc_clear_free_bit(...) == void");
 }
 
 // Quick and dirty method to find the smallest number pow such that 2^pow >= size.
