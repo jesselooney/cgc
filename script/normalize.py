886dc121570d8bf54e0b681c7cdf872acb9dc847
Madi Gudin <mgudin27@amherst.edu>
2025-11-06 11:56:09 -0500

    norm prints key to pointers

--- script/normalize.py
+++ script/normalize.py
@@ -10,8 +10,15 @@ while (l := sys.stdin.readline()):
 alpha = False if len(ptrs) > 26 else True 
 id = 'A' if alpha else 0
 
+subs = {}
+
 for p in ptrs:
+    subs[p] = id
     normalized = re.sub(p, f"< {id} >", normalized)
     id = chr(ord(id) + 1) if alpha else id + 1
 
+
 print(normalized)
+
+for k, v in sorted(subs.items()):
+    print(f"{v}\t{k}")
