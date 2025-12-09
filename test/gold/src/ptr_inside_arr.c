81c1b82cd9af1f91d197d660b96cf45acbe19ccd
Jesse Looney <jesselooney+dev@pm.me>
2025-12-08 19:29:54 -0500

    revamp goldens for pointer-inside support

---
+++ test/gold/src/ptr_inside_arr.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+
+#include "cgc.h"
+#include "debug.h"
+
+typedef struct list {
+    uint8_t head;
+    struct list *tail;
+} list_t;
+
+void list_t__map_ptrs(list_t *a, void (*f)(void *))
+{
+    (*f)(a->tail);
+}
+
+int main()
+{
+    cgc_scope_start();
+
+    cgc_scope_declare(list_t *, ptr);
+
+    cgc_scope_start();
+
+    cgc_scope_declare(list_t **, arr);
+    cgc_array(&arr, list_t *, 42);
+
+    for (int i = 0; i < 42; i++) {
+        arr[i] = NULL;
+    }
+
+    cgc_scope_declare(list_t *, list);
+    cgc_alloc(&list, list_t);
+    list->head = 0;
+    list->tail = NULL;
+
+    cgc_assign(&arr[31], list);
+
+    // `ptr` is a pointer _inside_ of `arr`.
+    cgc_assign(&ptr, &arr[13]);
+
+    // Drop `arr` and `list`.
+    cgc_scope_end();
+    cgc_collect();
+
+    // The objects should be kept alive here by `ptr`.
+    log_trace("allocated objects should not have been collected yet");
+
+    // Drop `ptr`, making the objects garbage.
+    cgc_scope_end();
+    cgc_collect();
+
+    log_trace("allocated objects should have been collected by now");
+
+    return 0;
+}
--- test/gold/src/ptr_inside_struct.c
+++ test/gold/src/ptr_inside_struct.c
@@ -23,9 +23,15 @@ int main()
 
     // Make a two-list chain a --> b.
     cgc_scope_declare(list_t *, a);
-    cgc_scope_declare(list_t *, b);
     cgc_alloc(&a, list_t);
+    a->head = 0;
+    a->tail = NULL;
+
+    cgc_scope_declare(list_t *, b);
     cgc_alloc(&b, list_t);
+    b->head = 1;
+    b->tail = NULL;
+
     cgc_assign(&a->tail, b);
 
     // `tail` is a pointer _inside_ of `a`.
